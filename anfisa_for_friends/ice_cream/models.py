838d170d2a8149dcecd2c79e5c54bdae2731cc32
Андрей Царенко <adochek@mail.ru>
2024-05-21 15:42:23 +0300

    last

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -56,10 +56,18 @@ class IceCream(PublishedModel):
         related_name='ice_creams',
         verbose_name='Категория',
     )
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     toppings = models.ManyToManyField(Topping, verbose_name='Топпинги')
     is_on_main = models.BooleanField(default=False, verbose_name='На главную')
+
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
+
     def __str__(self):
         return self.title
